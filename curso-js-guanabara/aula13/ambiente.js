6668c5acccd3b48b60dc51ffafe35962092f5962
igoroliveira21 <igoron182017@gmail.com>
2023-06-13 23:16:06 -0300

    while // do while

---
+++ curso-js-guanabara/aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2) 
+
+
+/*
+
+var c = 1
+while (c <= 6) {
+    console.log('tudo bem?')
+    c++  // c += 1 // c = c + 1
+}
+
+*/
