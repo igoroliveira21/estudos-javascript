0f5141b162fa0ec98ddc3c453c423f401076a39c
igoroliveira21 <igoron182017@gmail.com>
2023-06-15 00:32:27 -0300

    array em js

---
+++ curso-js-guanabara/vetores/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 4]
+num.push(1)
+num.sort()
+console.log(num.length)//comprimento do array
+console.log(`nosso vetor é o ${num}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
