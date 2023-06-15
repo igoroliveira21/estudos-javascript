7b5ad4e957f47c47ea3f086dd618a0278ba0658a
igoroliveira21 <igoron182017@gmail.com>
2023-06-15 00:35:53 -0300

    Update ambiente.js

--- curso-js-guanabara/vetores/ambiente.js
+++ curso-js-guanabara/vetores/ambiente.js
@@ -3,5 +3,9 @@ num.push(1)
 num.sort()
 console.log(num.length)//comprimento do array
 console.log(`nosso vetor é o ${num}`)
-let pos = num.indexOf(8)
-console.log(`O valor 8 está na posição ${pos}`)
+let pos = num.indexOf(6)
+if (pos == -1) {
+    console.log()
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
