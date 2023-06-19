99c040ca8b0b76a3c712230c0633802d555fd335
igoroliveira21 <igoron182017@gmail.com>
2023-06-19 11:04:05 -0300

    Create switch.js

---
+++ curso-udemy-js/condicoes/switch.js
@@ -0,0 +1,18 @@
+const imprimirResultado = function(nota) {
+    switch (Math.floor(nota)) {
+        case 10:
+        case 9:
+            console.log('Quadro de Honra')
+            break
+        case 8:
+        case 7:
+            console.log('Aprovado!')
+            break
+        case 6:
+        case 5:
+            console.log('Recuperação!')
+            break
+        case 4: case 3: case 2:
+    }
+}
+
