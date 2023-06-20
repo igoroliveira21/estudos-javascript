bf301aa1c52f5e9e748764a8dd474c640e60e318
igoroliveira21 <igoron182017@gmail.com>
2023-06-20 19:29:24 -0300

    callback no browser

--- curso-udemy-js/funcoes.js/callBack1.js
+++ curso-udemy-js/funcoes.js/callBack1.js
@@ -5,6 +5,4 @@ function imprimir(nome, indice) {
 }
 
 fabricantes.forEach(imprimir)
-fabricantes.forEach((fabricante) => {
-    console.log(fabricante)
-})
+fabricantes.forEach((fabricante) => { console.log(fabricante) })
