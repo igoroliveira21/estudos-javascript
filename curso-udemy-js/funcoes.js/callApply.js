5da356c55c352ba7536c26d4806be1d9231d4d5b
igoroliveira21 <igoron182017@gmail.com>
2023-06-21 20:32:28 -0300

    Update callApply.js

--- curso-udemy-js/funcoes.js/callApply.js
+++ curso-udemy-js/funcoes.js/callApply.js
@@ -18,3 +18,6 @@ const carro = {preco: 49990, desc: 0.20}
 
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
+
+console.log(getPreco.call(carro, 0.17, '$'))
+console.log(getPreco.apply(carro, [0.17, '$']))
