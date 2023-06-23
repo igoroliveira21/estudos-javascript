5e5b27f82da28bf1272747261a67c0c61f4e6be7
igoroliveira21 <igoron182017@gmail.com>
2023-06-23 19:02:33 -0300

    Object.value.key.assign
    forEach para percorrer os elementos chave e valor de um array

---
+++ curso-udemy-js/objeto/notacaoLiteral.js
@@ -0,0 +1,24 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj2 = {a, b, c}
+console.log(obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {
+    funcao1: function () {
+        //...
+    },
+    funcao2() {
+        // ...
+    }
+}   
+
+console.log(obj4)
