0d9d81295cccb899080543c362cd47e182de4ba7
igoroliveira21 <igoron182017@gmail.com>
2023-07-09 22:30:46 -0300

    querySelectorAll, querySelector

--- curso-cfb/aula33.js
+++ curso-cfb/aula33.js
@@ -1,13 +1,30 @@
+const divTodas = [...document.getElementsByTagName("div")]
 const cursosTodos = [...document.getElementsByClassName("curso")]
 const cursosC1 = [...document.getElementsByClassName("c1")]
 const cursosC2 = [...document.getElementsByClassName("c2")]
-const cursoEspecial =document.getElementsByClassName("curso")[6]
+const cursoEspecial =document.getElementById("c1")[6]
 
-console.log(cursosTodos)
-console.log(cursosC1)
-console.log(cursosC2)
-console.log(cursoEspecial)
+//const query_divTodas = [...document.querySelectorAll("div[class]")]
+const query_divTodas = [...document.querySelectorAll("div > p")]
+const query_cursosTodos =[...document.querySelectorAll(".curso")]
+const query_cursosC1 = [...document.querySelectorAll(".c1")]
+const query_cursosC2 = [...document.querySelectorAll(".c2")]
+const query_cursoEspecial =document.querySelectorAll("#c1")[0]
 
-cursosC2.map((e) => {
-    e.classList.add("destaque")
-} )
+
+
+console.log(query_divTodas)
+//console.log(query_cursosTodos)
+//console.log(query_cursosC1)
+//console.log(query_cursosC2)
+//console.log(query_cursoEspecial)
+
+// console.log(divTodas)
+// console.log(cursosTodos)
+// console.log(cursosC1)
+// console.log(cursosC2)
+// console.log(cursoEspecial)
+
+// cursosC2.map((e) => {
+//     e.classList.add("destaque")
+// })
