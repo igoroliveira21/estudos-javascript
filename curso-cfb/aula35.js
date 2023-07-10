918602db9da044780376902ae6039e28622ade89
igoroliveira21 <igoron182017@gmail.com>
2023-07-10 19:34:11 -0300

    addEventListener
    toggle

---
+++ curso-cfb/aula35.js
@@ -0,0 +1,28 @@
+const caixa1 = document.querySelector("#caixa1")
+const caixa2 = document.querySelector("#caixa2")
+const btn = document.querySelector("#btn_copiar")
+const btnv = document.querySelector("#btn_voltar")
+const todosCursos = [...document.querySelectorAll(".curso")]
+
+
+todosCursos.map((el)=>{
+    el.addEventListener("click", (evt) => {
+        const curso = evt.target
+        curso.classList.toggle("selecionado")
+
+    })
+})
+
+btn.addEventListener("click", () => {
+    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+    cursosSelecionados.map((el) => {
+        caixa2.appendChild(el)
+    })
+})
+
+btnv.addEventListener("click", () => {
+    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+    cursosSelecionados.map((el) => {
+        caixa1.appendChild(el)
+    })
+})
