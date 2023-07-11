46af2f305e93fdb21e3774cd97f2769034f00c13
igoroliveira21 <igoron182017@gmail.com>
2023-07-11 20:49:13 -0300

    setAttribute
    addEventListener
    removeChild(evt.target.parentNode)

--- curso-cfb/aula40.js
+++ curso-cfb/aula40.js
@@ -8,6 +8,14 @@ cursos.map((el, indice) => {
     novoElemento.setAttribute("id", `c${indice + 1}`)
     novoElemento.setAttribute("class", "curso c1")
     novoElemento.innerHTML = el
+    const btn_lixeira = document.createElement("img")
+    btn_lixeira.setAttribute("src","./lixeira.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    btn_lixeira.addEventListener("click", (evt) => {
+       caixa1.removeChild(evt.target.parentNode)
+    })
+
+    novoElemento.appendChild(btn_lixeira)
     caixa1.appendChild(novoElemento)
 })
 
