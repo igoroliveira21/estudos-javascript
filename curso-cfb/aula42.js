af76bdaa80f25255bcbc2596d7453e8632b26ad9
igoroliveira21 <igoron182017@gmail.com>
2023-07-12 19:57:05 -0300

    erro

--- curso-cfb/aula42.js
+++ curso-cfb/aula42.js
@@ -5,5 +5,12 @@ const maior = idades.filter((valor) => {
     }
 })
 
+const menor = idades.filter((valor) => {
+    if ( valor < 18) {
+        return valor
+    }
+})
+
 console.log(idades)
 console.log(maior)
+console.log(menor)
