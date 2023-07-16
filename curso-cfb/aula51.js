958190f7f158dcc4bbacff7048c256f496c60392
igoroliveira21 <igoron182017@gmail.com>
2023-07-15 21:28:42 -0300

    iterando texto em um array com Symbom.iterator

--- curso-cfb/aula51.js
+++ curso-cfb/aula51.js
@@ -1,10 +1,17 @@
 const valores=[10,8,9,2]
 const it_valores=valores[Symbol.iterator]()
 
-console.log(valores)
-console.log(it_valores.next().value)
-console.log(it_valores.next().value)
-console.log(it_valores.next().value)
-console.log(it_valores.next().value)
-console.log(it_valores.next().value)
+const texto="YouTube"
+const it_texto = texto[Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+
 
