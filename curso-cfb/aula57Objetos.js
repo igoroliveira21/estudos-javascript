02932719c0e4bd54440a094a21e73c67ad5a5722
igoroliveira21 <igoron182017@gmail.com>
2023-07-18 23:06:35 -0300

    Instanciando classes

--- curso-cfb/aula57Objetos.js
+++ curso-cfb/aula57Objetos.js
@@ -1,58 +1,49 @@
-class Carro{
-    constructor(pnome, ptipo){
+class Pessoa{
+    constructor(pnome, pidade){
         this.nome = pnome
-        if(ptipo==1) {
-            this.tipo = "Esportivo"
-            this.velmax = 300
-        } else if(ptipo==2){
-            this.tipo = "Utilitário"
-            this.velmax = 100
-        } else if(ptipo==3){
-            this.tipo = "Passeio"
-            this.velmax = 160
-        } else {
-            this.tipo = "Militar"
-            this.velmax = 180
-        }
+        this.idade = pidade
     }
     getNome() {
         return this.nome
     }
-    getTipo(){
-        return this.tipo
+    getIdade(){
+        return this.idade
     }
-    getVelMax(){
-        return this.velmax
-    }
-    getInfo(){
-        return [this.nome, this.tipo, this.velmax]
-    }
-
     setNome(nome){
         this.nome = nome
     }
-
+    setIdade(idade) {
+        this.idade = idade
+    }
     info() {
         console.log("Nome: " + this.nome)
-        console.log("Tipo: " + this.tipo)
-        console.log("V.Max: " + this.velmax)
+        console.log("Idade: " + this.idade)
         console.log("-----------------------")
     }
 }
 
-let c1 = new Carro("Rapidão", 1)
-let c2 = new Carro("Super Luxo", 2)
-let c3 = new Carro("Bombadão", 4)
-let c4 = new Carro("Carrego tudo", 3)
+let pessoas=[]
 
+const btn_add = document.querySelector("#btn_add")
+const res = document.querySelector(".res")
 
-console.log(c1.nome)
-console.log(c1.velmax)
-console.log(c1.tipo)
+const addPessoa=()=>{
+    res.innerHTML=""
+    pessoas.map((p)=>{
+        const div = document.createElement("div")
+        div.setAttribute("class", "pessoa")
+        div.innerHTML=`Nome:${p.getNome()}<br/>Idade:${p.idade}`
+        res.appendChild(div)
+    })
+}
 
-c1.setNome("Super Veloz")
-
-c1.info()
-// c2.info()
-
-// console.log(c1.getInfo())
+btn_add.addEventListener("click", (evt) => {
+    const nome = document.querySelector("#f_nome")
+    const idade = document.querySelector("#f_idade")
+    const p = new Pessoa(nome.value, idade.value)
+    pessoas.push(p)
+    nome.value=""
+    idade.value=""
+    nome.focus()
+    addPessoa()
+})
