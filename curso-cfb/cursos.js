8c83237708fea2cd8376a51d0a4ef32c4b99cc2a
igoroliveira21 <igoron182017@gmail.com>
2023-08-03 16:33:15 -0300

    modulos
    export {}
    
    import {} from "./cursos.js"

---
+++ curso-cfb/cursos.js
@@ -0,0 +1,4 @@
+const cursos = ['JavaScript', 'HTML', 'CSS', 'Arduino', "RaspBerry", 'C#', 'Python']
+const carros = ['Polo', 'T-Cross', 'Golf', 'Onix', 'Cruize', 'Argo', 'Cronnos']
+
+export {cursos, carros}
