12608aa994d4a772c48a38da45e5abeb2545c4ff
igoroliveira21 <igoron182017@gmail.com>
2023-08-07 17:49:22 -0300

    Update aula98Symbol.js

--- curso-cfb/aula98Symbol.js
+++ curso-cfb/aula98Symbol.js
@@ -5,7 +5,7 @@ class Jogador{
     }
 }
 
-let jogadores=[new Jogador("J1"), new Jogador("J2"), new Jogador("J3"), new Jogador("J4")]
+let jogadores=[new Jogador("J1"), new Jogador("J2"), new Jogador("J3"), new Jogador("J4"), new Jogador("J1")]
 
 // let s1 = jogadores[3].id
 let s = []
@@ -18,5 +18,11 @@ s=s_jogadores.map((j)=>{
     return j.id
 })
 
+r_simbolos = jogadores.filter((j)=>{
+    return j.nome!="J1"
+})
+
+console.log(r_simbolos)
+console.log(jogadores)
 console.log(s_jogadores)
 console.log(s)
