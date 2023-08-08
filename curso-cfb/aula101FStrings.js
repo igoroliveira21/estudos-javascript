ba6a3b92b8eaa0c6ed6733ca1622de0cdff2eae7
igoroliveira21 <igoron182017@gmail.com>
2023-08-07 22:04:47 -0300

    Create aula101FStrings.js

---
+++ curso-cfb/aula101FStrings.js
@@ -0,0 +1,21 @@
+let nome = new String("Igor Pinho Oliveira")
+let canal = new String("CFB Cursos")
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))//
+console.log(nome.charAt(1))
+console.log(nome.charAt(2))
+console.log(nome.charAt(3))
+
+console.log(nome.charCodeAt(0))// Retorna o código do Elemento I
+
+console.log(nome.concat(canal))
+
+console.log(nome.concat(canal))
+console.log(nome)
+
+console.log(nome.indexOf("Ig"))
+
+console.log(nome.lastIndexOf("i"))
