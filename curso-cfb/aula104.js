63c14b45b8deabdecae35c02ef72f2b14d60b0af
igoroliveira21 <igoron182017@gmail.com>
2023-08-09 16:58:48 -0300

    charCodeAt()
    String.fromCodePoint()

--- curso-cfb/aula104.js
+++ curso-cfb/aula104.js
@@ -10,3 +10,16 @@ console.log(nome.includes("Oli"))//Procura em alguma parte da string a string pa
 
 console.log(nome.repeat("3"))//Repete a stringa tantas vezes
 console.log(nome.valueOf())
+
+console.log(nome.charCodeAt(0))//passa o código de cada elemento seguinte da posição tal
+console.log(nome.charCodeAt(1))
+console.log(nome.charCodeAt(2))
+console.log(nome.charCodeAt(3))
+
+
+console.log(String.fromCodePoint(73, 103, 111, 114))//Pega o código de cada letra/ código ask
+
+
+let nome_ts="Igor"
+
+console.log(`Nome: ${nome_ts}`)
