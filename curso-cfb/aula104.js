2a0beba402dee5743003c8c3da2433647a0d0471
igoroliveira21 <igoron182017@gmail.com>
2023-08-09 16:55:15 -0300

    includes()
    valueOf()
    repeat()

---
+++ curso-cfb/aula104.js
@@ -0,0 +1,12 @@
+let nome = new String("Igor de Oliveira Nobrega")
+
+console.log(nome.startsWith("I"))
+
+console.log(nome.endsWith("a"))
+
+console.log(nome.includes("Oli"))//Procura em alguma parte da string a string passada
+
+
+
+console.log(nome.repeat("3"))//Repete a stringa tantas vezes
+console.log(nome.valueOf())
