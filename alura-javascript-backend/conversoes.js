cacc1dbfd98e07c608f0c88c222702d57ed409af
igoroliveira21 <igoron182017@gmail.com>
2023-08-28 22:19:19 -0300

    conversoes, tipos boleanos e variáveis

---
+++ alura-javascript-backend/conversoes.js
@@ -0,0 +1,10 @@
+// tipo de dado
+// booleanos
+
+// conversao implícita
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero + numeroString)
+
+// conversao explícita
--- alura-javascript-backend/type-boolean.js
+++ alura-javascript-backend/type-boolean.js
@@ -10,3 +10,10 @@ const texto1 = "Alura";
 const texto2 = "a";
 
 console.log(texto1 === texto2)
+
+// const minhaVar = 1;
+// const MinhaVar = "texto";
+// const minhavar = "3";
+// const MINHAVAR = 2;
+
+// console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
