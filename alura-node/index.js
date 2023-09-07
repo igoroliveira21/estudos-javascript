0e2fac286f61b03e6e4d38cc91681cb9bce41da1
igoroliveira21 <igoron182017@gmail.com>
2023-09-07 18:55:37 -0300

    importando libs
    chalk
    fs

--- alura-node/index.js
+++ alura-node/index.js
@@ -1,7 +1,19 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Olá Mundo!'))
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {//a função callback traz o resultado do readFile
+        if (erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
