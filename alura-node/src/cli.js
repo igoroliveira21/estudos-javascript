321a0d37249465166d1cd0c2308cfe0ab07f2ea1
igoroliveira21 <igoron182017@gmail.com>
2023-09-11 22:22:36 -0300

    funções assincronas

---
+++ alura-node/src/cli.js
@@ -0,0 +1,25 @@
+import chalk from "chalk";
+import fs from 'fs';
+import pegaArquivo from "./index.js";
+
+const caminho = process.argv;
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        console.log(chalk.yellow('Lista de links:'), resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+         arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            console.log(`${caminho}/${nomeDeArquivo}`);
+            console.log(lista);
+        })
+        console.log(arquivos) 
+    }
+
+}
+
+processaTexto(caminho);
