dba12a464cc3e39fe92a91e1f29657c108a3ff04
igoroliveira21 <igoron182017@gmail.com>
2023-09-13 21:29:12 -0300

    Try/ catch

--- alura-node/src/cli.js
+++ alura-node/src/cli.js
@@ -1,23 +1,46 @@
 import chalk from "chalk";
 import fs from 'fs';
 import pegaArquivo from "./index.js";
+import { resourceUsage } from "process";
 
 const caminho = process.argv;
+
+function imprimeLista(resultado, identificador = '') {
+
+    if (valida) {
+        console.log(
+            chalk.yellow('lista validada'),
+            chalk.black.bgGreen(identificador),
+            listaValidada(resultado));
+    }      
+    console.log(
+        chalk.yellow('lista validada'),
+        chalk.black.bgGreen(identificador),
+        resultado);  
+}
+ 
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
 
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log('Arquivo ou diretório não existe.')
+            return
+        }
+    }
+
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(argumentos[2]);
-        console.log(chalk.yellow('Lista de links:'), resultado);
+        imprimeLista(resultado);
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
          arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            console.log(`${caminho}/${nomeDeArquivo}`);
-            console.log(lista);
+            imprimeLista(lista, nomeDeArquivo);
         })
-        console.log(arquivos) 
     }
 
 }
