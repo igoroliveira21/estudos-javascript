a6535d9894987c3f5584c57bb6b03f742d5bfe14
igoroliveira21 <igoron182017@gmail.com>
2023-09-20 22:51:43 -0300

    metodos de acesso em classes privadas

--- alura-javascript-orientado-a-objetos/index.js
+++ alura-javascript-orientado-a-objetos/index.js
@@ -4,5 +4,8 @@ import Admin from './Admin.js';
 
 const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
 console.log(novoUser.exibirInfos())
-
 // novoUser.#email = 'Márcia'; propriedade privada
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '1998-01-01')
+console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.nomeAdmin())
